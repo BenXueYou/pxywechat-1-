451f168bbc282da186ade9abf41b05c1fe206396
pxyhdljdylzsx <1048356176@qq.com>
2017-02-23 09:37:56 +0800

    查看refund文件

--- html/SMCharging/aaa.php
+++ html/SMCharging/aaa.php
@@ -2,25 +2,25 @@
 
 
 
-require_once "lib/WxPay.Api.php"; 
-   
-$out_trade_no = "143933040220170215152532";
-$refund_fee="1";
-$total_fee = "1";
-		
+require_once "lib/WxPay.Api.php";
+
+$out_trade_no = "143933040220170221123307";
+$refund_fee="100";
+$total_fee = "100";
+
 
 
     $input = new WxPayRefund();
     $input->SetOut_trade_no($out_trade_no);
-	$input->SetTotal_fee($total_fee);
-	$input->SetRefund_fee($refund_fee);
+	  $input->SetTotal_fee($total_fee);
+	  $input->SetRefund_fee($refund_fee);
     $input->SetOut_refund_no(WxPayConfig::MCHID.date("YmdHis"));
     $input->SetOp_user_id(WxPayConfig::MCHID);
 
 	$order = WxPayApi::refund($input);
 
-    var_dump($order);	
-	
+    var_dump($order);
+
 echo "123";
 
 ?>
